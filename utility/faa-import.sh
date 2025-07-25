6d3ab719302cb902002dfc3ef8b3b219295eaf8b
Diggy Bell <diggy@diggybell.com>
2025-07-24 19:32:20 -0500

    Fixed but in FAA import bash script

--- utility/faa-import.sh
+++ utility/faa-import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/php faa-import.php --directory=../faa/ --file=MASTER.txt --truncate --import > mysql faa
-/usr/bin/php faa-import.php --directory=../faa/ --file=RESERVED.txt --truncate --import > mysql faa
-/usr/bin/php faa-import.php --directory=../faa/ --file=ACFTREF.txt --truncate --import > mysql faa
-/usr/bin/php faa-import.php --directory=../faa/ --file=ENGINE.txt --truncate --import > mysql faa
+/usr/bin/php faa-import.php --directory=../faa/ --file=MASTER.txt --truncate --import > faa-import.sql ; mysql faa < faa-import.sql
+/usr/bin/php faa-import.php --directory=../faa/ --file=RESERVED.txt --truncate --import >faa-import.sql ; mysql faa < faa-import.sql
+/usr/bin/php faa-import.php --directory=../faa/ --file=ACFTREF.txt --truncate --import >faa-import.sql ; mysql faa < faa-import.sql
+/usr/bin/php faa-import.php --directory=../faa/ --file=ENGINE.txt --truncate --import >faa-import.sql ; mysql faa < faa-import.sql
